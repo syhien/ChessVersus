553c295b7b7c6f994c4ab5539a3efd8f92b09c2c
syhien <syhien@outlook.at>
2020-11-07 21:00:47 +0800

    main()流程开始构建，添加空模板配置

--- main.cpp
+++ main.cpp
@@ -3,11 +3,54 @@
 #include "status.h"
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
 using namespace std;
+
+const string ask_for_check = "（按y确认，按其他按键取消）\n";
+const string kick_to_continue = "按任意按键继续\n";
+
+Player player;
+vector <Record> record;
+
+void GameInitialize();
+void Login();
+void AdminPanel();
 
 int main()
 {
+	system("cls");
+	srand(time(0));
+	GameInitialize();
+	Login();
 
 	cout << "\nUnfinished project\n";
 	return 0;
+}
+
+void GameInitialize()
+{
+	ifstream fin;
+	fin.open("game.data");
+	if (!fin)
+		fin.open("game_template.data");
+	if (!fin)//全新初始化
+	{
+
+	}
+	else//有配置
+	{
+
+	}
+}
+
+void Login()
+{
+
+}
+
+void AdminPanel()
+{
+
 }
