7009a6335540e5104bde5fb8d031d81e43cb3b12
syhien <syhien@outlook.at>
2020-11-08 11:10:01 +0800

    实现加载管理员密码

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <algorithm>
+#include <conio.h>
 using namespace std;
 
 const string ask_for_check = "（按y确认，按其他按键取消）\n";
@@ -13,6 +14,8 @@ const string kick_to_continue = "按任意按键继续\n";
 
 Player player;
 vector <Record> record;
+
+string admin_password;
 
 void GameInitialize();
 void Login();
@@ -37,12 +40,18 @@ void GameInitialize()
 		fin.open("game_template.data");
 	if (!fin)//全新初始化
 	{
-
+		cout << "输入新管理员密码:\n（空格及之后的内容会被忽略）";
+		cin >> admin_password;
+		fflush(stdin);
+		cout << "新管理员密码为：" << admin_password << endl;
+		cout << kick_to_continue, _getch();
 	}
 	else//有配置
 	{
+		fin >> admin_password;
 
 	}
+	fin.close();
 }
 
 void Login()
